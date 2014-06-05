29211fb5103cceeb1dda915e910406cce6e85f85
Felicia Wang <felis_3007@yahoo.com>
2014-06-05 15:50:18 -0700

    Adding some more validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -29,21 +29,54 @@ describe "Creating todo lists"  do
 		expect(page).to_not have_content("This is what I'm doing today.")
 	end
 
-
-	it "displays error when the to do list is too short" do
+	it "displays error when the title is too short (< 3 chars)" do
 		expect(TodoList.count).to eq(0)
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
-		fill_in "Title", with: "Short todo list"
-		fill_in "Description", with: "hi"
+		fill_in "Title", with: "hi"
+		fill_in "Description", with: "I need to go to grocery store."
 		click_button "Create Todo list"
 
 		expect(TodoList.count).to eq(0)
 		expect(page).to have_content("error")
 
 		visit "/todo_lists"
-		expect(page).to_not have_content("hi")
+		expect(page).to_not have_content("I need to go to grocery store.")
+	end
+
+	it "displays error when the to do list is blank" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Short todo list"
+		fill_in "Description", with: " "
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Short todo list")
+	end
+
+	it "displays error when the description is too short (< 5 chars)" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Grocery list"
+		fill_in "Description", with: "Food"
+		click_button "Create Todo list"
+
+		expect(TodoList.count).to eq(0)
+		expect(page).to have_content("error")
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Food")
 	end
 end
