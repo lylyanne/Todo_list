1740eb7d74d87905d8dc3c179732be0a2ef4d69c
Felicia Wang <felis_3007@yahoo.com>
2014-06-11 18:04:43 -0700

    Add feature to edit todo items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -10,14 +10,36 @@ describe "Creating todo items" do
 		end
 	end
 
+	def update_todo_item(content="Milk")
+		click_link "New Todo Item"
+		fill_in "Content", with: content
+		click_button "Save"
+	end
+
 	it "is successful with valid content" do
 		visit_todo_list(todo_list)
-		click_link "New Todo Item"
-		fill_in "Content", with: "Milk"
-		click_button "Save"
+		update_todo_item
 		expect(page).to have_content("Added todo list item.")
 		within "ul.todo_items" do
 			expect(page).to have_content("Milk")
 		end
+	end
+
+	it "displays an error with no content" do
+		visit_todo_list(todo_list)
+		update_todo_item("")
+		within "div.flash" do
+			expect(page).to have_content("There was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content can't be blank")
+	end
+
+	it "displays an error if content is less than 3 characters" do
+		visit_todo_list(todo_list)
+		update_todo_item("eg")
+		within "div.flash" do
+			expect(page).to have_content("There was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content is too short")
 	end
 end
---
+++ spec/features/todo_items/edit_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Creating todo items" do
+	let!(:todo_list) {TodoList.create(title: "Groceries", description: "Tissue paper and towel") }
+	let!(:todo_item) {todo_list.todo_items.create(content: "Milk")}
+	
+	def visit_todo_list(list) 
+		visit '/todo_lists'
+		within "#todo_list_#{list.id}" do
+			click_link "List Items"	
+		end
+	end
+
+	it "is successful with valid content" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: "Soap"
+		click_button "Save"
+		expect(page).to have_content("Saved todo list item.")
+		todo_item.reload
+		expect(todo_item.content).to eq("Soap")
+	end
+
+	it "fails with no content" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: ""
+		click_button "Save"
+		expect(page).to_not have_content("Saved todo list item.")
+		expect(page).to have_content("Content can't be blank")
+		todo_item.reload
+		expect(todo_item.content).to eq("Milk")
+	end
+
+	it "fails with content that is too short" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Edit"
+		end
+		fill_in "Content", with: "Eg"
+		click_button "Save"
+		expect(page).to_not have_content("Saved todo list item.")
+		expect(page).to have_content("Content is too short")
+		todo_item.reload
+		expect(todo_item.content).to eq("Milk")
+	end
+
+end
