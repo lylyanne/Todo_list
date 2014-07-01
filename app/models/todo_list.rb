b2d914781bbcdca581b6e586581891fa29853f2b
Felicia Wang <felis_3007@yahoo.com>
2014-06-30 17:20:11 -0700

    Adding completed and incomplete items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,4 +4,12 @@ class TodoList < ActiveRecord::Base
 	validates :title, :description, presence: true
 	validates :title, length: {minimum: 3 }	
 	validates :description, length: {minimum: 5 }	
+
+	def has_completed_items?
+		todo_items.complete.size > 0
+	end
+
+	def has_incomplete_items?
+		todo_items.incomplete.size > 0
+	end
 end
